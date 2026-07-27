ebb7d7d47c0536caca6df0359a313f2de504d375
sajafaansari123-cmyk <sajafaansari123@gmail.com>
2026-07-27 21:18:19 +0500

    Add files via upload

---
+++ Function class assignment.py
@@ -0,0 +1,35 @@
+print("FUNCTION ASSIGNMENT")
+print("Q1")
+def list(name):
+  print(len(name))
+list(["sajafa","Sara","Ahmed"])
+print("=========================")
+print("Q2")
+def length(fruits):
+  for fruit in fruits:
+    print(fruit)
+length(["mango","apple","orange"])
+print("=========================")
+print("Q3")
+def multiply(num1,num2):
+  result=2*3
+  print(result)
+multiply(2,3)  
+print("=========================")
+print("Q4")
+def convert(pkr):
+  rate = 285
+  usd=pkr/rate
+  print("USD=",usd)
+amount = 45
+convert(amount)
+print("=========================")
+print("Q5")
+def check(num):
+  if num%2==0:
+    print("even")
+  else:
+    print("odd")
+check(12)
+print("=========================")
+  
